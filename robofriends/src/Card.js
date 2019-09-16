17f2d66d071770eec1b95dc5cff9c6dc1585abc9
fsansalvadore <f.sansalvadore@gmail.com>
2019-09-16 19:35:13 +0200

    Populated robot cardlist with robots.map() loop

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,29 +1,14 @@
 import React from 'react';
 
-const card = () => {
+
+const card = ({name, email, id}) => {
     return (
-        // <div>
-        //     <img src="" alt="robot"></img>
-        //     <h1>Robot Name</h1>
-        //     <p>robot.email@gmail.com</p>
-        // </div>
-        <div>
-            <div class="row">
-            <div class="col s6 m3 l2">
-            <div class="card">
-                <div class="card-image">
-                    <img src="https://robohash.org/lol" />
-                    <span class="card-title">Card Title</span>
-                    </div>
-                    <div class="card-content">
-                    <p>Robot Brown</p>
-                    </div>
-                    <div class="card-action">
-                    <a href="#">This is a link</a>
-                    </div>
-                </div>
-            </div>
-            </div>
+        <div className="bg-light-green tc dib br3 ma2 pa3 grow bw-2 shadow-5">
+            <img src={`https://robohash.org/"${id}"?150x150`}/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>   
         </div>
     )
 }
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
 import * as serviceWorker from './serviceWorker';
-import 'materialize-css/dist/css/materialize.min.css';
+import 'tachyons';
+import App from './App';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <App />
+    </div>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
